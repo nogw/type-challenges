e6e849e3d747dba9741ef9b5082189d7b7e51276
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-06 15:27:34 -0300

    challenge 05

---
+++ src/challenge-05.ts
@@ -0,0 +1,13 @@
+import { A } from "ts-toolbelt";
+import { Equal, Expect } from "./utils";
+
+type First<T extends unknown[]> = T extends [infer First, ...infer _]
+  ? First
+  : never;
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+];
