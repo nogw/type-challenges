6b7bb4b79d2c3871968ed874357b5b2f359e39fd
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-07 09:57:29 -0300

    challenge 23

---
+++ src/challenge-23.ts
@@ -0,0 +1,15 @@
+import { Equal, Expect } from "./utils";
+
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{ [A in keyof T]: T[A] extends Promise<infer B> ? B : T[A] }>;
+
+const promiseAllTest1 = PromiseAll([1, 2, 3] as const);
+const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const);
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)]);
+
+type cases = [
+  Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
+  Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
+  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>
+];
