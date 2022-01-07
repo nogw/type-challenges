5e4ba574be621125e6a567848af7182c8753eff8
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-06 22:20:21 -0300

    challenge 09

---
+++ src/challenge-09.ts
@@ -0,0 +1,8 @@
+import { Equal, Expect } from "./utils";
+
+type If<C, T, F> = C extends true ? T : F;
+
+type cases = [
+  Expect<Equal<If<true, "a", "b">, "a">>,
+  Expect<Equal<If<false, "a", 2>, 2>>
+];
