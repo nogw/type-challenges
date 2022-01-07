f5b59c8d45d901025cd0b2c86b9dab6f9ae7c925
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-07 11:06:50 -0300

    challenge 28

---
+++ src/challenge-28.ts
@@ -0,0 +1,23 @@
+import { Equal, Expect } from "./utils";
+
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ""
+  ? S
+  : S extends `${infer A}${From}${infer B}`
+  ? `${A}${To}${ReplaceAll<B, From, To>}`
+  : S;
+
+type cases = [
+  Expect<Equal<ReplaceAll<"foobar", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobar", "bag", "foo">, "foobar">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "bar", "foo">, "foofoofoo">>,
+  Expect<Equal<ReplaceAll<"t y p e s", " ", "">, "types">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "", "foo">, "foobarbar">>,
+  Expect<Equal<ReplaceAll<"barfoo", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobarfoobar", "ob", "b">, "fobarfobar">>,
+  Expect<Equal<ReplaceAll<"foboorfoboar", "bo", "b">, "foborfobar">>,
+  Expect<Equal<ReplaceAll<"", "", "">, "">>
+];
