924aa0a3d03504e4676fc3d5e9e102a5fd259f05
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-06 22:35:59 -0300

    challenge 10

---
+++ src/challenge-10.ts
@@ -0,0 +1,15 @@
+import { Equal, Expect } from "./utils";
+
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<
+    Equal<
+      Concat<["1", 2, "3"], [false, boolean, "4"]>,
+      ["1", 2, "3", false, boolean, "4"]
+    >
+  >
+];
